68b5a1e9992951821c099ea516cf2404363faa00
dali <dali@ufofactory.org>
2015-12-17 12:13:47 +0900

    edit, update, destroy canoes

--- app/controllers/canoes_controller.rb
+++ app/controllers/canoes_controller.rb
@@ -1,15 +1,21 @@
 class CanoesController < ApplicationController
 
+  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :fetch_canoe, only: [:show, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update, :destroy]
+
   def index
     @canoes = Canoe.all
   end
 
   def show
-    @canoe = Canoe.find params[:id]
   end
 
   def new
     @canoe = Canoe.new
+  end
+
+  def edit
   end
 
   def create
@@ -22,7 +28,27 @@ class CanoesController < ApplicationController
     end
   end
 
+  def update
+    @canoe.update(canoe_params)
+    redirect_to @canoe
+  end
+
+  def destroy
+    @canoe.destroy
+    redirect_to @canoe
+  end
+
   private
+
+  def fetch_canoe
+    @canoe ||= Canoe.find params[:id]
+  end
+
+  def correct_user
+    unless @canoe.user == current_user
+      redirect_to(@canoe)
+    end
+  end
 
   def canoe_params
     params.require(:canoe).permit(:title)
--- test/integration/canoes_test.rb
+++ test/integration/canoes_test.rb
@@ -10,7 +10,39 @@ class CanoesTest < ActionDispatch::IntegrationTest
     sign_in users(:one)
     post canoes_path(canoe: { title: 'test'} )
 
-    assert users(:one), assigns(:canoe).user
-    assert 'test', assigns(:canoe).title
+    assert_equal users(:one), assigns(:canoe).user
+    assert_equal 'test', assigns(:canoe).title
+  end
+
+  test 'edit by owner' do
+    sign_in users(:one)
+
+    title = canoes(:canoe1).title + 'xx'
+    put canoe_path(id: canoes(:canoe1).id, canoe: { title: title})
+
+    assert_equal title, assigns(:canoe).title
+  end
+
+  test 'should not edit a canoe by the other' do
+    sign_in users(:two)
+
+    origin_title = canoes(:canoe1).title
+    put canoe_path(id: canoes(:canoe1).id, canoe: { title: origin_title + 'xx'})
+
+    assert_equal origin_title, assigns(:canoe).title
+  end
+
+  test 'delete' do
+    sign_in users(:one)
+    delete canoe_path(id: canoes(:canoe1).id)
+
+    refute Canoe.exists?(canoes(:canoe1).id)
+  end
+
+  test 'should not destory a canoe by the other' do
+    sign_in users(:two)
+    delete canoe_path(id: canoes(:canoe1).id)
+
+    assert Canoe.exists?(canoes(:canoe1).id)
   end
 end
