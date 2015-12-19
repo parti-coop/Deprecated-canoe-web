2ecbe63e222e942c1836fbdcce69d18f182417a1
Roy Jung <phi4grv@gmail.com>
2015-12-19 22:13:41 +0900

    Verify owner creating canoe in canoe spec

--- spec/features/canoe_spec.rb
+++ spec/features/canoe_spec.rb
@@ -4,10 +4,13 @@ feature 'Canoe' do
   include_context 'auth'
   include_context 'canoe'
 
-  scenario 'Create new canoe' do
+  scenario 'Create new canoe by valid user' do
     sign_in_as 'valid-user@email.com'
     create_canoe title: 'Canoe created by scenario'
-    canoe_should_be_created title: 'Canoe created by scenario'
+    canoe_should_be_created(
+      title: 'Canoe created by scenario',
+      user: { email: 'valid-user@email.com' }
+    )
   end
 
 end
--- spec/support/canoe_context.rb
+++ spec/support/canoe_context.rb
@@ -8,6 +8,10 @@ shared_context 'canoe' do
 
   def canoe_should_be_created(attrs)
     canoe = Canoe.find Canoe.last_created.id
+    if attrs.has_key? :user
+      user = attrs.delete(:user)
+      expect(user.stringify_keys.to_a - canoe.user.attributes.to_a).to be_empty
+    end
     expect(attrs.stringify_keys.to_a - canoe.attributes.to_a).to be_empty
   end
 
