17bb87e58577fa62e7dbd1991debabe5dadf89bc
dali <dali@ufofactory.org>
2015-12-21 22:03:07 +0900

    cleanup test case

--- test/integration/canoes_test.rb
+++ test/integration/canoes_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class CanoesTest < ActionDispatch::IntegrationTest
-  test 'list' do
-    get canoes_path
-    refute assigns(:canoes).nil?
-  end
-
   test 'new' do
     sign_in users(:one)
     post canoes_path(canoe: { title: 'test', theme: 'theme'} )
