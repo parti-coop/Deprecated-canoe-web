6e2f104967c3cb684095e3a2c4d86df10e97a682
dali <dali@ufofactory.org>
2016-01-12 22:47:48 +0900

    order discussions by discussed_at in canoe page

--- test/integration/unread_test.rb
+++ test/integration/unread_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UnreadTest < ActionDispatch::IntegrationTest
-  focus
   test 'unread discussion' do
     sign_in users(:one)
     post discussion_opinions_path(discussion_id: discussions(:discussion1).id, opinion: { body: 'test' } )
