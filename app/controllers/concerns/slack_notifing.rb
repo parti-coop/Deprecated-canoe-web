2cc902e4b2db50da05ca3e17ab07231b659bbcc7
rest515 <rest515@ufofactory.org>
2016-01-15 23:40:58 +0900

    change slack message

--- app/controllers/concerns/slack_notifing.rb
+++ app/controllers/concerns/slack_notifing.rb
@@ -14,18 +14,18 @@ module SlackNotifing
 
     message = make_message(object)
     if message.present?
-      notifier.ping("[Canoe!] #{message[:title]}", attachments: [{ text: message[:body], color: "#36a64f" }])
+      notifier.ping("[Canoe!!:[#{canoe.title}](#{canoe_url canoe})] #{message[:title]}", attachments: [{ text: message[:body], color: "#36a64f" }])
     end
   end
 
   private
 
   def fetch_webhook_url(object)
-    canoe = object if object.class == Canoe
-    canoe ||= object.discussion.canoe if object.respond_to?(:discussion)
-    canoe ||= object.canoe if object.respond_to?(:canoe)
+    @canoe = object if object.class == Canoe
+    @canoe ||= object.discussion.canoe if object.respond_to?(:discussion)
+    @canoe ||= object.canoe if object.respond_to?(:canoe)
 
-    canoe.try(:slack_webhook_url)
+    @canoe.try(:slack_webhook_url)
   end
 
   def make_message(object)
@@ -48,57 +48,57 @@ module SlackNotifing
     when "discussions#create"
       discussion = object
       title = "@#{current_user.nickname}님이 논의를 만들었습니다."
-      body = "[#{discussion.subject}](#{discussion_url discussion}) >>> #{discussion.body}"
+      body = "논의: [#{discussion.subject}](#{discussion_url discussion}) >>> #{discussion.body}"
     when "discussions#update"
       discussion = object
       title = "@#{current_user.nickname}님이 논의를 고쳤습니다."
-      body = "[#{discussion.subject}](#{discussion_url discussion}) >>> #{discussion.body}"
+      body = "논의: [#{discussion.subject}](#{discussion_url discussion}) >>> #{discussion.body}"
     when "discussions#destroy"
       discussion = object
       title = "@#{current_user.nickname}님이 논의를 지웠습니다."
-      body = "#{discussion.subject} >>> #{discussion.body}"
+      body = "논의: #{discussion.subject} >>> #{discussion.body}"
     when "proposals#create"
       proposal = object
       discussion = proposal.discussion
       title = "@#{current_user.nickname}님이 제안을 만들었습니다."
-      body = "[#{discussion.subject}](#{discussion_url discussion}) >>> #{proposal.body}"
+      body = "#{proposal.body}\n논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "proposals#update"
       proposal = object
       discussion = proposal.discussion
       title = "@#{current_user.nickname}님이 제안을 고쳤습니다."
-      body = "[#{discussion.subject}](#{discussion_url discussion}) >>> #{proposal.body}"
+      body = "#{proposal.body}\n논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "proposals#destroy"
       proposal = object
       discussion = proposal.discussion
       title = "@#{current_user.nickname}님이 제안을 지웠습니다."
-      body = "#{discussion.subject} >>> #{proposal.body}"
+      body = "#{proposal.body}\n논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "opinions#create"
       opinion = object
       discussion = opinion.discussion
       title = "@#{current_user.nickname}님이 의견을 올렸습니다."
-      body = "[#{discussion.subject}](#{opinion_url opinion}) >>> #{opinion.body}"
+      body = "#{opinion.body}\n논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "opinions#update"
       opinion = object
       discussion = opinion.discussion
       title = "@#{current_user.nickname}님이 의견을 고쳤습니다."
-      body = "[#{discussion.subject}](#{opinion_url opinion}) >>> #{opinion.body}"
+      body = "#{opinion.body}\n논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "opinions#destroy"
       opinion = object
       discussion = opinion.discussion
       title = "@#{current_user.nickname}님이 의견을 지웠습니다."
-      body = "#{discussion.subject} >>> #{opinion.body}"
+      body = "#{opinion.body}\n논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "votes#in_favor", "votes#opposed"
       vote = object
       proposal = vote.proposal
       discussion = proposal.discussion
       title = "@#{current_user.nickname}님이 #{proposal.body} 제안에 투표합니다."
-      body = "[#{discussion.subject}](#{discussion_url discussion})"
+      body = "논의: [#{discussion.subject}](#{discussion_url discussion})"
     when "votes#unvote"
       vote = object
       proposal = vote.proposal
       discussion = proposal.discussion
       title = "@#{current_user.nickname}님이 #{proposal.body} 제안에 투표철회합니다."
-      body = "[#{discussion.subject}](#{discussion_url discussion})"
+      body = "논의: [#{discussion.subject}](#{discussion_url discussion})"
     else
       nil
     end
