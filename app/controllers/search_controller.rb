30052f380817b4771012cee0ce32dded49217015
dali <dali@ufofactory.org>
2016-02-02 21:54:45 +0900

    seach canoes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,18 +6,23 @@ class SearchController < ApplicationController
       if model.blank?
         @canoes = Canoe.search_for(params[:q])
         @canoes_count = @canoes.count
-        @canoes_on_current_page = @canoes.limit(6)
+        @canoes_on_current_page = @canoes.limit(3)
 
         @discussions = Discussion.valid_parent.order(discussed_at: :desc).search_for(params[:q])
         @discussions_count = @discussions.count
         @discussions_on_current_page = @discussions.limit(5)
       else
-        klass = model.singularize.capitalize.constantize
-        @models = klass.search_for(params[:q]).page(params[:page])
-        @models = @models.valid_parent if @models.respond_to?(:valid_parent)
-        @models_count = @models.count
-        @models_on_current_page = @models.page(params[:page])
-        render template: "search/#{model.pluralize}"
+        if model == 'canoe'
+          @canoes = Canoe.search_for(params[:q])
+          @canoes_count = @canoes.count
+          @canoes_on_current_page = @canoes.page(params[:page])
+          render template: "search/canoes"
+        elsif model == 'discussion'
+          @discussions = Discussion.valid_parent.order(discussed_at: :desc).search_for(params[:q])
+          @discussions_count = @discussions.count
+          @discussions_on_current_page = @discussions.page(params[:page])
+          render template: "search/discussions"
+        end
       end
     end
   end
