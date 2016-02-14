056e4e366b6e7610939b6ef690406e0602b3b7b2
dali <dali@ufofactory.org>
2016-02-14 22:28:30 +0900

    show joined canoe for current user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,15 @@ class PagesController < ApplicationController
   def home
     session[SSO_RETURN_TO_KEY] = root_url
 
-    limit = 30
+    limit_count = 30
     if user_signed_in?
-      @discussions = current_user.joined_discussions.order(discussed_at: :desc).first(limit)
+      @discussions = current_user.joined_discussions.order(discussed_at: :desc).first(limit_count)
       @canoes = current_user.joined_canoes
+      @canoes = Canoe.limit(limit_count) if @canoes.empty?
       current_user.touch_home
       current_user.save
     end
 
-    if @canoes
-      @canoes = Canoe.limit(20)
-    end
+    @canoes ||= Canoe.limit(limit_count)
   end
 end
