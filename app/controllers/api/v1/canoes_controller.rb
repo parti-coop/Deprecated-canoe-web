a9ecf1d2175e81a4261216f8d72432c3c6a05a26
dali <dali@ufofactory.org>
2016-03-30 17:02:54 +0900

    joined canoes to canoe api

--- app/controllers/api/v1/canoes_controller.rb
+++ app/controllers/api/v1/canoes_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::CanoesController < Api::V1::BaseController
   def show
     @canoe = Canoe.find params[:id]
-    result = @canoe.serializable_hash include: {
+    canoe_hash = @canoe.serializable_hash include: {
       user: {},
       crews: { include: [:user] },
       request_to_joins: { include: [:user] },
       invitations: { include: [:user] },
       request_to_joins: { include: [:user] }
     }
-    result.update discussions: hashed_discussions_with_newest(@canoe.discussions)
-    expose(result)
+    canoe_hash.update discussions: hashed_discussions_with_newest(@canoe.discussions)
+
+    expose({
+      joined_canoes: current_user.joined_canoes,
+      canoe: canoe_hash
+    })
   end
 end
 
