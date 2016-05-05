731722c1cfd1bd6bc469a38d8d2547e25c15f4e7
dali <dalikim@parti.xyz>
2016-05-05 15:35:40 +0900

    iso push

--- app/services/push_sender.rb
+++ app/services/push_sender.rb
@@ -3,6 +3,10 @@ class PushSender
 
   def initialize
     @android_pusher = GCM.new(ENV['GCM_API_KEY'])
+    @ios_pusher = Grocer.pusher(
+      certificate: Rails.root.join('config/apns.pem'),
+      gateway: (Rails.env.production? ? "gateway.sandbox.push.apple.com" : "gateway.push.apple.com")
+    )
   end
 
   def push_now(user, message)
@@ -10,6 +14,18 @@ class PushSender
       if device.device_platform == 'android'
         options = {data: {alert: message}, collapse_key: "akey"}
         response = @android_pusher.send([device.device_id], options)
+      elsif user.device_os == 'ios'
+        notification = Grocer::Notification.new(
+          device_token:      user.device_id,
+          alert:             message,
+          # badge:             42,
+          # category:          "a category",         # optional; used for custom notification actions
+          # sound:             "siren.aiff",         # optional
+          # expiry:            Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
+          # identifier:        1234,                 # optional; must be an integer
+          # content_available: true                  # optional; any truthy value will set 'content-available' to 1
+        )
+        @ios_pusher.push(notification)
       end
     end
   end
